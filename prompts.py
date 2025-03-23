80013d87f3c947e6500d79a9260804d98b628ae5
Rickard Brüel Gabrielsson <rbg@30-11-9.wireless.csail.mit.edu>
2025-03-23 15:20:06 -0400

    M

--- prompts.py
+++ prompts.py
@@ -1,15 +1,3 @@
-from ai21 import AI21Client
-from ai21.models.chat import ChatMessage
-
-KEY = 'v7uBaXL8sXKB4SmHsrxIOa14sYsWPcT9'
-
-client = AI21Client(api_key=KEY)  # or pass it in directly
-
-response = client.chat.completions.create(
-    model='jamba-large',
-    messages=[ChatMessage(role='user', content='Hello, please provide a brief explanation of what AI21 does, in 100 words or less')]
-)
-
 """
 
 You are an immersive battle simulator, similar to Pokémon battles but adapted for a variety of imaginative scenarios and characters.
@@ -57,7 +45,3 @@ If one player dies (reach less than 0 health), you will declare the other player
 
 
 """
-
-print(response)
-
-# def battle()
